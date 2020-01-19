36ebc558ea4376a868b1ffe62d5392db644ef0d9
Maxim Sovkov <maxim.sovkov@saritasa.com>
2020-01-19 19:00:09 +0700

    Add the path model, implement route builder logic

---
+++ RouteBuilder/Path.cs
@@ -0,0 +1,26 @@
+﻿namespace RouteBuilder
+{
+    /// <summary>
+    /// Path model.
+    /// </summary>
+    public class Path
+    {
+        /// <summary>
+        /// The starting point of the path.
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// The endpoint of the path.
+        /// </summary>
+        public string To { get; set; }
+
+        /// <summary>
+        /// Get a convenient representation of the path.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{{ {From}, {To} }}";
+        }
+    }
+}
---
+++ RouteBuilder/RouteBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RouteBuilder
+{
+    /// <summary>
+    /// Route builder.
+    /// </summary>
+    public class RouteBuilder
+    {
+        /// <summary>
+        /// Build a route based on input paths.
+        /// </summary>
+        /// <param name="paths">Paths.</param>
+        public string BuildRoute(List<Path> paths)
+        {
+            // Sort paths by the logical route.
+            for (var i = 0; i < paths.Count; i++)
+            {
+                for (var j = 0; j < paths.Count; j++)
+                {
+                    if (paths[i].From == paths[j].To && j > i)
+                    {
+                        var temp = paths[i];
+                        paths[i] = paths[j];
+                        paths[j] = temp;
+                    }
+                }
+            }
+
+            // Collect the output array by taking the "From" field from all elements,
+            // and the "From" and "To" fields from the last element.
+            var result = paths.Select((path, index) =>
+            {
+                if (index == paths.Count - 1)
+                {
+                    return $"{path.From} - {path.To}";
+                }
+
+                return path.From;
+            });
+
+            // Combine the output array into a separate line representing the route.
+            return string.Join(" - ", result);
+        }
+    }
+}
