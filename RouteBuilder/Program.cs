40aa6da1ed9d0ec81caa0e46bdc33c078e643570
Maxim Sovkov <maxim.sovkov@saritasa.com>
2020-01-19 19:38:16 +0700

    Implement console application logic

--- RouteBuilder/Program.cs
+++ RouteBuilder/Program.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RouteBuilder
 {
+    /// <summary>
+    /// Defines the entry point to the application.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        static void Main()
         {
+            // Add support for the Russian language.
+            Console.OutputEncoding = Console.InputEncoding = Encoding.GetEncoding("Cyrillic");
+
+            var paths = new List<Path>();
+
+            while (true)
+            {
+                Console.WriteLine("Пожалуйста, введите количество путей.");
+                if (!int.TryParse(Console.ReadLine(), out var pathCount) || pathCount <= 0)
+                {
+                    Console.WriteLine("Введённое Вами число некорректно!");
+                    Console.Write(Environment.NewLine);
+                }
+                else
+                {
+                    for (var i = 0; i < pathCount; i++)
+                    {
+                        Console.Write(Environment.NewLine);
+                        Console.WriteLine($"Путь номер {i + 1}.");
+
+                        Console.Write("From: ");
+                        var from = Console.ReadLine();
+
+                        Console.Write("To: ");
+                        var to = Console.ReadLine();
+
+                        paths.Add(new Path
+                        {
+                            From = from,
+                            To = to,
+                        });
+                    }
+
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Исходные данные:");
+                    paths.ForEach(path => Console.WriteLine(path.ToString()));
+
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Выходные данные:");
+
+                    var result = new RouteBuilder().BuildRoute(paths);
+                    Console.WriteLine(result);
+                    Console.Write(Environment.NewLine);
+
+                    paths.Clear();
+                }
+            }
         }
     }
 }
