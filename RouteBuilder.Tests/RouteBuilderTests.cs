1de001b907e1b226d7b03580a100f97d3c8f2b01
Maxim Sovkov <maxim.sovkov@saritasa.com>
2020-01-19 19:02:08 +0700

    Add unit test

---
+++ RouteBuilder.Tests/RouteBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace RouteBuilder.Tests
+{
+    /// <summary>
+    /// Route builder unit tests.
+    /// </summary>
+    [TestFixture]
+    public class RouteBuilderTests
+    {
+        /// <summary>
+        /// Make sure the route is built correctly.
+        /// </summary>
+        [Test]
+        public void EnsureRouteCorrectness()
+        {
+            var paths = new List<Path>
+            {
+                new Path { From = "Москва", To = "Тюмень" },
+                new Path { From = "Тюмень", To = "Сочи" },
+                new Path { From = "Ростов-на-Дону", To = "Москва" },
+            };
+
+            var actualRoute = new RouteBuilder().BuildRoute(paths);
+            var expectedRoute = "Ростов-на-Дону - Москва - Тюмень - Сочи";
+
+            Assert.AreEqual(expectedRoute, actualRoute);
+        }
+    }
+}
